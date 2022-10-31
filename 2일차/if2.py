2f4e66fe6ea7837afea2f38d87ca122ca65cb654
tk <kkom_bom@naver.com>
2022-10-31 17:51:10 +0900

    파이썬

--- 2일차/if2.py
+++ 2일차/if2.py
@@ -1,10 +1,30 @@
 '''
 파이썬에서만 가능
-'''
+# '''
 
-a = 10
-b=20
-c=30
+# a = 10
+# b=10
+# c=10
+# if a==b==c : 
+#     print("같다")
 
-if a==b==c : 
-    print("같다")
+# score =[3,6,10,5,7]
+# maxNum = max(a,b,c)
+# mimNum = min(score)
+
+from random import randint
+
+from macpath import split
+
+
+# a,b,c = map(int(),input().split(' '))
+# a = int[a]
+# b = int[b]
+# c = int[c]
+
+arr = input(),split(' ')
+for a in arr:
+    a= int[a]
+
+
+https://www.acmicpc.net/problem/2480 // 백준문제 풀기;
